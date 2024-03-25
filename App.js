11a3d0a3c9f2abfd24f485afefe1a2f083620536
Raabiya Rismy <136999950+Raabi-ya@users.noreply.github.com>
2024-03-26 02:40:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PomodoroTimer from './PomodoroTimer'; // Assuming PomodoroTimer.js is in the same directory
+
+function App() {
+  return (
+    <div className="App">
+      <PomodoroTimer />
+    </div>
+  );
+}
+
+export default App;
